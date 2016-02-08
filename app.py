ee18c02260a7ba4dc39d0bba1d7ab003c67b07cb
John Boy <jboy+git@sdf.org>
2016-02-09 00:33:14 +0100

    Cleaned up handling of config file, began py3 compatibility.

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python2.7
-from ConfigParser import SafeConfigParser
 from optparse import OptionParser
+try:
+    from ConfigParser import SafeConfigParser
+except ImportError:
+    from configparser import SafeConfigParser
 from bottle import route, post, run, request, view, response, static_file
 from sh import cmus_remote
 
@@ -18,25 +21,27 @@ option_parser.add_option('-p', '--app-port', dest='app_port',
                          help='cmus_app port', default=8080)
 
 
+class ConfigFileNotFound(IOError):
+    pass
+
+
+class MissingSetting(Exception):
+    pass
+
+
 def read_config(config_file):
     r = {}
     config_parser = SafeConfigParser()
     n = config_parser.read(config_file)
     if not len(n):
-        raise IOError('File not found: {}'.format(config_file))
+        raise ConfigFileNotFound(config_file)
     section = 'cmus_app'
-    required = ['cmus_host', 'cmus_passwd']
-    for S in required:
+    fields = ['cmus_host', 'cmus_passwd', 'app_host', 'app_port']
+    for field in fields:
         try:
-            r[S] = config_parser.get(section, S)
+            r[field] = config_parser.get(section, field)
         except:
-            raise ValueError('{} does not specify {}.'.format(config_file, S))
-    optional = [('app_host', r['cmus_host']), ('app_port', 8080)]
-    for S in optional:
-        try:
-            r[S[0]] = config_parser.get(section, S[0])
-        except:
-            r[S[0]] = S[1]
+            raise MissingSetting(field)
     return r
 
 
